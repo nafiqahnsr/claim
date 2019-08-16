de3fa847f2f4fd8810b495d0e787105ec08fc2bf
Mohd Fazli <fazli@mindwave.my>
2019-08-16 14:58:58 +0800

    Added mydb class & fixed dashboard

---
+++ cuticlaim/class/mydbvb.vb
@@ -0,0 +1,63 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class mydb
+    Public Shared Property _errStatus As Boolean = False
+    Public Shared Property _errMsg As String = ""
+    Public Property constr As String
+    Shared conn As MySqlConnection
+    Public Property DbName As String
+
+    Public Sub New()
+        constr = ConfigurationManager.ConnectionStrings("constr").ConnectionString
+        Try
+            conn = New MySqlConnection(constr)
+            conn.Open()
+            DbName = conn.Database
+        Catch ex As Exception
+            _errStatus = True
+            _errMsg = ex.Message
+        End Try
+    End Sub
+
+    Public Sub Close()
+        If conn.State = ConnectionState.Open Then conn.Close()
+    End Sub
+
+    Public Function CreateCommand() As MySqlCommand
+        If conn.State <> ConnectionState.Open Then conn.Open()
+        Return conn.CreateCommand()
+    End Function
+
+    Public Function Execute(vSQL As String) As Boolean
+        Dim tStatus As Boolean = True
+        If vSQL = "" Then
+            _errStatus = True
+            _errMsg = "No query statement to execute"
+            Return Nothing
+        End If
+        Try
+            Dim cmd As MySqlCommand = CreateCommand()
+            cmd.CommandText = vSQL
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            _errStatus = True
+            _errMsg = ex.Message
+            tStatus = False
+        End Try
+        Return tStatus
+    End Function
+
+    Public Function Search(vSQL As String) As DataTable
+        Dim dt As New DataTable()
+        If vSQL = "" Then Return Nothing
+        Try
+            Dim cmd As MySqlCommand = CreateCommand()
+            cmd.CommandText = vSQL
+            Dim sda As New MySqlDataAdapter(cmd)
+            sda.Fill(dt)
+        Catch ex As Exception
+            _errStatus = True
+            _errMsg = ex.Message
+        End Try
+        Return dt
+    End Function
+End Class
--- cuticlaim/cuticlaim/Dashboard.aspx.vb
+++ cuticlaim/cuticlaim/Dashboard.aspx.vb
@@ -1,7 +1,4 @@
-﻿Imports System.Data
-Imports System.Configuration
-Imports MySql.Data.MySqlClient
-Public Class Dashboard
+﻿Public Class Dashboard
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -11,20 +8,11 @@ Public Class Dashboard
     End Sub
 
     Private Sub BindGridView()
-        Dim claim As String = ConfigurationManager.ConnectionStrings("claim").ConnectionString
-        Using con As New MySqlConnection(claim)
-            Using cmd As New MySqlCommand("select NAMA, STAFF_ID, ROLE_JAWATAN, EMEL, TEL1 from tbl_staff;")
-                Using sda As New MySqlDataAdapter()
-                    cmd.Connection = con
-                    sda.SelectCommand = cmd
-                    Using dt As New DataTable()
-                        sda.Fill(dt)
-                        GridView1.DataSource = dt
-                        GridView1.DataBind()
-                    End Using
-                End Using
-            End Using
-        End Using
+        Dim mydb As New mydb
+        Dim dt As New DataTable()
+        dt = mydb.Search("select NAMA, STAFF_ID, ROLE_JAWATAN, EMEL, TEL1 from tbl_staff;")
+        GridView1.DataSource = dt
+        GridView1.DataBind()
     End Sub
 
     Protected Sub GridView1_PageIndexChanging(ByVal sender As Object, ByVal e As GridViewPageEventArgs) Handles GridView1.PageIndexChanging
